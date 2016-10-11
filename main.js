0a80b14f576b34cabf6bb29462c0d085fa8a1400
LaDonna <boxgirl85@gmail.com>
2016-10-11 11:41:31 -0500

    initial js

---
+++ main.js
@@ -0,0 +1,83 @@
+var productArr = [];
+var  ballPit= {
+	name: "Ball Pit Desk",
+	description: "When you need a break, fall off your chair for some fun!" ,
+	price: "$799", 
+	image url: "images/ball-pit-desk.jpg",
+	button: "Add to Cart"
+}
+
+var  carDesk= {
+	name: "Car Desk",
+	description: "This is for the car lovers out there.",
+	price: "$2399", 
+	image url:"images/car-desk.jpg",
+	button: "Add to Cart"
+
+}
+
+var childDesk = {
+	name: "Child Desk" ,
+	description: "Keep small children entertained.",
+	price: "$32", 
+	image url:"images/child-desk.jpg",
+	button: "Add to Cart"
+
+}
+
+var gardenDesk = {
+	name: "Garden Desk",
+	description: "Now you don't have to go outside for Zen.",
+	price: "$231", 
+	image url: "images/garden-desk.jpg",
+	button: "Add to Cart"
+
+}
+
+var lieDownDesk = {
+	name: "Lie Down Desk",
+	description: "Don't like being normal? This will help.",
+	price: "$4595", 
+	image url: "images/lie-down-desk.jpg",
+	button: "Add to Cart"
+
+}
+
+var outsideDesk = {
+	name: "Outside Desk",
+	description:"This is our MVP desk for outdoorsy people." ,
+	price:"$11" , 
+	image url:"images/outside-desk",
+	button: "Add to Cart"
+
+}
+
+var tieFighterDesk = {
+	name: "Tie Fighter Desk",
+	description:"It's Star Wars related so you have to buy it." ,
+	price:"$8399" , 
+	image url:"images/tie-fighter-desk",
+	button: "Add to Cart"
+
+}
+
+var toiletDesk = {
+	name: "Toilet Desk",
+	description: "Never leave your desk for maximum efficiency.",
+	price: "$210", 
+	image url:"images/toilet-desk",
+	button: "Add to Cart"
+
+}
+
+var  treadmillDesk= {
+	name: "Treadmill Desk",
+	description: "Don't die young. Walk and type!",
+	price: "$1119", 
+	image url: "images/treadmill-desk.jpg",
+	button: "Add to Cart"
+
+}
+
+productArr.push(treadmillDesk);
+produc
